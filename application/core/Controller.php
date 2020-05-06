8bc231566ff4dff454bfa0531da089b6064033a3
tanjuha <kovalenko.tetiana.oleksandrivna@gmail.com>
2020-05-07 02:34:23 +0300

    + access control

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -8,9 +8,13 @@ abstract class Controller {
 
   public $route;
   public $view;
+  public $acl;
 
 	public function __construct($route) {
     $this->route = $route;
+    if(!$this->checkAcl()) {
+      View::errorCode(404);
+    }
     $this->view = new View($route);
     $this->model = $this->loadModel($route['controller']);
 
@@ -21,6 +25,24 @@ abstract class Controller {
     if(class_exists($path)) {
       return new $path;
     }
+  }
+
+  public function checkAcl() {
+    $this->acl = require 'application/acl/'.$this->route['controller'].'.php';
+    if($this->isAcl('all')) {
+      return true;
+    } else if(isset($_SESSION['authorize']['id']) && $this->isAcl('authorize') ) {
+      return true;
+    } else if(!isset($_SESSION['authorize']['id']) && $this->isAcl('guest') ) {
+      return true;
+    } else if(isset($_SESSION['admin']) && $this->isAcl('admin') ) {
+      return true;
+    }
+    return false;
+  }
+
+  public function isAcl($key) {
+    return in_array($this->route['action'],  $this->acl[$key]);
   }
 
 }
