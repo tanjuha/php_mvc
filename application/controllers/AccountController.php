395b0285933716582befde322360f6363330c634
tanjuha <kovalenko.tetiana.oleksandrivna@gmail.com>
2020-05-07 01:07:55 +0300

    + ajax, + function message, + function location

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -7,6 +7,11 @@ use application\core\Controller;
 class AccountController extends Controller {
 
   public function loginAction() {
+
+    if(!empty($_POST)) {
+     // $this->view->message('success', 'text');
+      $this->view->location('/');
+    }
      //$this->view->redirect("/");
     $this->view->render("Login page");
   }
--- application/views/account/register.php
+++ application/views/account/register.php
@@ -1,20 +1,20 @@
 <div class="row justify-content-center">
   <div class="col-6 mt-4">
-  <form class= "border p-3">
+  <form class= "border p-3" action="/account/login" method="post">
   <h3>Register page</h3>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4">
+      <label for="inputEmail4">Name</label>
+      <input type="text" class="form-control" id="inputEmail4" name="name">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4">
+      <input type="password" class="form-control" id="inputPassword4" name="password">
     </div>
   </div>
   <div class="form-group">
     <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" name="address">
   </div>
   <div class="form-group">
     <div class="form-check">
